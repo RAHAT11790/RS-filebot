48ba27fc874f15769d324e4478c6a25485c60c11
RAHAT11790 <rahatsarker224@gmail.com>
2025-10-01 19:51:08 +0600

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-API_ID = int(os.getenv("API_ID"))        # টেলিগ্রাম থেকে পাওয়া
-API_HASH = os.getenv("API_HASH")         # টেলিগ্রাম থেকে পাওয়া
-BOT_TOKEN = os.getenv("BOT_TOKEN")       # BotFather থেকে পাওয়া
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")   # Render domain, যেমন: https://mybot.onrender.com/webhook
